5948f94c5dcb418706d3647d7e2dc7159d78e517
CardyP <Paolo.cardarella@gmail.com>
2025-11-01 18:23:43 -0400

    Refactor SQL retail sales analysis script

--- Retail Data Analysis.sql
+++ Retail Data Analysis.sql
@@ -20,7 +20,7 @@ CREATE TABLE retail_sales
             );
 
 SELECT * FROM retail_sales
-LIMIT 10
+
 
 
     
@@ -161,3 +161,57 @@ GROUP
     category,
     gender
 ORDER BY 1
+
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+SELECT
+	year,
+	month,
+	avg_sale
+FROM
+(
+	SELECT
+		EXTRACT(YEAR FROM sale_date) as year,
+		EXTRACT(MONTH FROM sale_date) as month,
+		AVG(total_sale) as avg_sale,
+		RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank
+	FROM retail_sales
+	GROUP BY 1, 2
+) as t1
+WHERE rank = 1
+-- ORDER BY 1, 2
+
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+SELECT
+	customer_id as best_custy,
+	SUM(total_sale) as total_spent
+FROM retail_sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5
+
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+SELECT
+      category,
+      COUNT(DISTINCT customer_id) as cnt_unique_cs
+FROM retail_sales
+GROUP BY CATEGORY
+
+-- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
+WITH hourly_sales
+AS
+(
+SELECT *,
+         CASE
+	WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
+	WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+	ELSE 'Eveneing'
+         END as shift
+FROM retail_sales
+)
+SELECT
+      shift,
+      COUNT(*) as total_orders
+FROM hourly_sales
+GROUP BY shift
+
+-- End of Project
